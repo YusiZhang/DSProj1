7eede43f0957ac0f62833fe18155654061726221
YusiZhang <yusi.leo.zhang@gmail.com>
2014-05-28 19:40:11 -0400

    none

---
+++ DS-Proj-1/src/lab1/TestProcess.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class TestProcess implements MigratableProcess{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suspend() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String toString() {
+		return "this is a test process";
+	}
+
+}
